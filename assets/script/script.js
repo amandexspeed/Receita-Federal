b274b3e31d476897bdf4df8080ffba26352d5eb6
Amanda de Jesus Melo <60000299+amandexspeed@users.noreply.github.com>
2023-12-28 18:48:44 -0300

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,36 @@
+function validarCnpj(cnpj){
+
+    $.ajax({
+
+        'url':'https://receitaws.com.br/v1/cnpj/' + cnpj.replace(/[^0-9]/g,''),
+        'type':'GET',
+        'dataType': 'jsonp',
+        'success': function(dado)
+        {
+
+            if(dado.nome == undefined){
+
+                alert(dado.status + '' + dado.message)
+
+            }else{
+
+
+                document.getElementById('razaosocial').value = dado.nome
+                document.getElementById('nomeFant').value = dado.fantasia
+                document.getElementById('abertura').value = dado.abertura
+                document.getElementById('situacao').value = dado.status
+                document.getElementById('endereco').value = `${dado.logradouro},${dado.numero}`
+                document.getElementById('municipio').value = dado.municipio
+                document.getElementById('estado').value = dado.uf
+                document.getElementById('telefone').value = dado.telefone
+
+
+            }
+
+            
+            
+        }
+
+    })
+
+}
